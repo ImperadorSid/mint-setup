dfd35ba8244f0cd3353e5ba7cff311db7577dc08
Moisés Carvalho <moisescarv27@hotmail.com>
2020-02-27 12:08:20 -0300

    Add reset settings script

---
+++ reset-settings.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+dconf reset -f /com/linuxmint/updates/
+dconf reset -f /com/linuxmint/report/
+dconf reset -f /com/gexperts/Tilix/
+dconf reset -f /org/x/viewer/
+dconf reset -f /org/cinnamon/
+dconf reset -f /org/gnome/
+dconf reset -f /org/nemo/
